b1500c8fc2d4f4e26500a5d86a737ee7643b4423
Andrew Timberlake <andrew@andrewtimberlake.com>
2017-06-27 19:59:08 +0200

    Add helper methods to State to allow saving and restoring state

--- lib/trunk/state.ex
+++ lib/trunk/state.ex
@@ -19,7 +19,8 @@ defmodule Trunk.State do
             storage_opts: [],
             errors: nil,
             assigns: %{}
-  @type t :: %__MODULE__{module: atom, opts: Keyword.t, filename: String.t, rootname: String.t, extname: String.t, path: String.t, versions: list(atom) | Keyword.t, async: boolean, version_timeout: integer, scope: map | struct, storage: atom, storage_opts: Keyword.t, errors: Keyword.t, assigns: map}
+  @type opts :: Keyword.t
+  @type t :: %__MODULE__{module: atom, opts: opts, filename: String.t, rootname: String.t, extname: String.t, path: String.t, versions: list(atom) | Keyword.t, async: boolean, version_timeout: integer, scope: map | struct, storage: atom, storage_opts: Keyword.t, errors: Keyword.t, assigns: map}
 
   def init(%{} = info, scope, opts) do
     filename = info[:filename]
@@ -72,4 +73,112 @@ defmodule Trunk.State do
   @spec assign(state :: Trunk.State.t, key :: any, value :: any) :: map
   def assign(%{assigns: assigns} = state, key, value),
     do: %{state | assigns: Map.put(assigns, key, value)}
+
+  @doc ~S"""
+  Extracts the data needed from the state in order to reconstruct the file paths in future.
+
+  Options:
+  - `:as` - How to save the state.
+    - `:string` - Default, will just save the file name. An error will be raised if there are any assigns unless `:ignore_assigns` is set to tru
+    - `:map` - will save a map with keys `:filename`, `:assigns`, and `:version_assigns`
+    - `:json` - will save a map encoded as JSON
+  - `:ignore_assigns` boolean, default false. Use this to save as string and ignore any assigns (Make sure you’re not using assigns for `c:Trunk.storage_dir/2` or `c:Trunk.filename/2`)
+  - `:assigns` - a list of keys to save from the assigns hashes
+
+  ## Example:
+  ```
+  iex> Trunk.State.save(%Trunk.State{filename: "photo.jpg"})
+  "photo.jpg"
+  iex> Trunk.State.save(%Trunk.State{filename: "photo.jpg", assigns: %{hash: "abcdef"}}, as: :map)
+  %{filename: "photo.jpg", assigns: %{hash: "abcdef"}}
+  iex> Trunk.State.save(%Trunk.State{filename: "photo.jpg", assigns: %{hash: "abcdef", file_size: 12345}}, as: :map, assigns: [:hash])
+  %{filename: "photo.jpg", assigns: %{hash: "abcdef"}}
+  iex> Trunk.State.save(%Trunk.State{filename: "photo.jpg", assigns: %{hash: "abcdef"}}, as: :json)
+  "{\"filename\": \"photo.jpg\", \"assigns\": {\"hash\": \"abcdef\"}}"
+  ```
+  """
+  @type assign_keys :: [atom]
+  @type save_opts :: [assigns: :all | assign_keys]
+  @spec save(Trunk.State.t) :: String.t
+  @spec save(Trunk.State.t, [{:as, :string} | save_opts]) :: String.t
+  @spec save(Trunk.State.t, [{:as, :json} | save_opts]) :: String.t
+  @spec save(Trunk.State.t, [{:as, :map} | save_opts]) :: map
+  def save(state, opts \\ []) do
+    save_as = Keyword.get(opts, :as, :string)
+    save_as(state, save_as, opts)
+  end
+
+  defp save_as(%{filename: filename} = state, :string, opts) do
+    unless Keyword.get(opts, :ignore_assigns, false), do: assert_no_assigns(state)
+    filename
+  end
+  defp save_as(state, :json, opts), do: state |> save_as(:map, opts) |> Poison.encode!
+  defp save_as(%{filename: filename, assigns: assigns, versions: versions}, :map, opts),
+    do: %{filename: filename,
+          assigns: save_assigns(assigns, Keyword.get(opts, :assigns, :all)),
+          version_assigns: versions |> Enum.map(fn({version, %{assigns: assigns}}) ->
+            {version, save_assigns(assigns, Keyword.get(opts, :assigns, :all))}
+          end) |> Map.new}
+
+  defp assert_no_assigns(%{assigns: assigns}) when assigns != %{},
+    do: raise ArgumentError, message: "Cannot save state as string with non-empty assigns hash"
+  defp assert_no_assigns(%{versions: versions}),
+    do: Enum.each(versions, fn({_version, state}) -> assert_no_assigns(state) end)
+  defp assert_no_assigns(%{}), do: nil
+
+  defp save_assigns(map, :all), do: map
+  defp save_assigns(map, keys), do: Map.take(map, keys)
+
+  @doc ~S"""
+  Restore a saved state from a filename, JSON, or a map
+
+  ## Example:
+  ```
+  iex> Trunk.State.restore("photo.jpg")
+  %Trunk.State{filename: "photo.jpg"}
+  iex> Trunk.State.restore(%{filename: "photo.jpg", assigns: %{hash: "abcdef"}}
+  %Trunk.State{filename: "photo.jpg", assigns: %{hash: "abcdef"}}
+  iex> Trunk.State.restore(%{"filename" => "photo.jpg", "assigns" => %{"hash" => "abcdef"}}
+  %Trunk.State{filename: "photo.jpg", assigns: %{hash: "abcdef"}}
+  iex> Trunk.State.restore("{\"filename\": \"photo.jpg\", \"assigns\": {\"hash\": \"abcdef\"}}")
+  %Trunk.State{filename: "photo.jpg", assigns: %{hash: "abcdef"}}
+  ```
+  """
+  @type file_info :: String.t | map
+  @spec restore(file_info) :: Trunk.State.t
+  def restore(file_info)
+  def restore(<<"{", _rest::binary>> = json) do
+    {:ok, map} = Poison.decode(json)
+    map
+    |> keys_to_atom
+    |> restore
+
+  end
+  def restore(<<filename::binary>>),
+    do: %__MODULE__{filename: filename}
+  def restore(%{version_assigns: version_assigns} = info) do
+    state = struct(__MODULE__, info)
+    versions =
+      version_assigns
+      |> Enum.map(fn({version, assigns}) ->
+        {version, %VersionState{assigns: assigns}}
+      end)
+      |> Map.new
+    %{state | versions: versions}
+  end
+  def restore(%{} = map), do: map |> keys_to_atom |> restore
+
+  defp keys_to_atom(%{} = map) do
+    map
+    |> Enum.map(fn({key, value}) ->
+      try do
+        {String.to_existing_atom(key), keys_to_atom(value)}
+      rescue
+        ArgumentError ->
+          {key, keys_to_atom(value)}
+      end
+    end)
+    |> Map.new
+  end
+  defp keys_to_atom(arg), do: arg
 end
--- test/state_test.exs
+++ test/state_test.exs
@@ -1,7 +1,7 @@
 defmodule Trunk.StateTest do
   use ExUnit.Case, async: true
 
-  alias Trunk.State
+  alias Trunk.{State, VersionState}
 
   test "put_error/4" do
     state = State.put_error(%State{}, :thumb, :transform, "Invalid option")
@@ -11,5 +11,89 @@ defmodule Trunk.StateTest do
   test "assign/3" do
     state = State.assign(%State{}, :my_key, :my_value)
     assert state.assigns[:my_key] == :my_value
+  end
+
+  describe "save/2" do
+    test "saves the filename by default" do
+      state = %State{filename: "test.jpg"}
+      assert State.save(state) == "test.jpg"
+    end
+
+    test "saves the filename" do
+      state = %State{filename: "test.jpg"}
+      assert State.save(state, as: :string) == "test.jpg"
+    end
+
+    test "raise error if attempting to save as string with assigns" do
+      state = %State{filename: "test.jpg", assigns: %{hash: "abcdef"}}
+      assert_raise ArgumentError, fn ->
+        State.save(state, as: :string)
+      end
+
+      state = %State{filename: "test.jpg", versions: %{original: %{assigns: %{hash: "abcdef"}}}}
+      assert_raise ArgumentError, fn ->
+        State.save(state, as: :string)
+      end
+    end
+
+    test "do not raise error if attempting to save as string with assigns and ignore_assigns is set" do
+      state = %State{filename: "test.jpg", assigns: %{file_size: 12345}}
+      assert State.save(state, as: :string, ignore_assigns: true) == "test.jpg"
+
+      state = %State{filename: "test.jpg", versions: %{original: %{assigns: %{file_size: 12345}}}}
+      assert State.save(state, as: :string, ignore_assigns: true) == "test.jpg"
+    end
+
+    test "saves a full hash with all assigns" do
+      state = %State{filename: "test.jpg", assigns: %{hash: "abcdef"}, versions: %{original: %VersionState{assigns: %{hash: "fedcba"}}, thumb: %VersionState{assigns: %{hash: "abcdfe"}}}}
+      assert State.save(state, as: :map) == %{filename: "test.jpg", assigns: %{hash: "abcdef"}, version_assigns: %{original: %{hash: "fedcba"}, thumb: %{hash: "abcdfe"}}}
+    end
+
+    test "saves a full hash with only required assigns" do
+      state = %State{filename: "test.jpg", assigns: %{hash: "abcdef", file_size: 1234}, versions: %{original: %VersionState{assigns: %{hash: "fedcba", file_size: 1234}}, thumb: %VersionState{assigns: %{hash: "abcdfe", file_size: 123}}}}
+      assert State.save(state, as: :map, assigns: [:hash]) == %{filename: "test.jpg", assigns: %{hash: "abcdef"}, version_assigns: %{original: %{hash: "fedcba"}, thumb: %{hash: "abcdfe"}}}
+    end
+
+    test "saves a full hash with all assigns as json" do
+      state = %State{filename: "test.jpg", assigns: %{hash: "abcdef"}, versions: %{original: %VersionState{assigns: %{hash: "fedcba"}}, thumb: %VersionState{assigns: %{hash: "abcdfe"}}}}
+      assert Poison.decode!(State.save(state, as: :json)) == Poison.decode!(~S({"filename":"test.jpg","assigns":{"hash":"abcdef"},"version_assigns":{"thumb":{"hash":"abcdfe"},"original":{"hash":"fedcba"}}}))
+    end
+  end
+
+  describe "restore/2" do
+    test "restores from a filename" do
+      assert %State{filename: "my_file.ext"} = State.restore("my_file.ext")
+    end
+
+    test "restores from a map" do
+      assert %State{filename: "my_file.ext",
+                    assigns: %{hash: "abcdef"},
+                    versions: %{original: %VersionState{assigns: %{hash: "fedcba"}},
+                                thumb: %VersionState{assigns: %{hash: "abcdfe"}}}} =
+        State.restore(%{filename: "my_file.ext", assigns: %{hash: "abcdef"},
+                        version_assigns: %{
+                           original: %{hash: "fedcba"},
+                           thumb: %{hash: "abcdfe"}}})
+    end
+
+    test "restores from a map with string keys" do
+      assert %State{filename: "my_file.ext",
+                    assigns: %{hash: "abcdef"},
+                    versions: %{original: %VersionState{assigns: %{hash: "fedcba"}},
+                                thumb: %VersionState{assigns: %{hash: "abcdfe"}}}} =
+        State.restore(%{"filename" => "my_file.ext", "assigns" => %{"hash" => "abcdef"},
+                        "version_assigns" => %{
+                          "original" => %{"hash" => "fedcba"},
+                          "thumb" => %{"hash" => "abcdfe"}}})
+    end
+
+    test "restores from a json string" do
+      json = ~S({"filename":"my_file.ext","assigns":{"hash":"abcdef"},"version_assigns":{"thumb":{"hash":"abcdfe"},"original":{"hash":"fedcba"}}})
+      assert %State{filename: "my_file.ext",
+                    assigns: %{hash: "abcdef"},
+                    versions: %{original: %VersionState{assigns: %{hash: "fedcba"}},
+                                thumb: %VersionState{assigns: %{hash: "abcdfe"}}}} =
+        State.restore(json)
+    end
   end
 end
