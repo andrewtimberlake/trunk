223999c24dd6ebc9f65a9cb8ec547725af88486c
Andrew Timberlake <andrew@andrewtimberlake.com>
2017-07-03 15:16:16 +0200

    Release 0.0.4

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Trunk.Mixfile do
   def project do
     [
       app: :trunk,
-      version: "0.0.3",
+      version: "0.0.4",
       elixir: "~> 1.4",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
